2f48038d06475c2a827194f173b1c4fa3ada762b
lecture-bisper <bisper@outlook.kr>
2022-04-28 11:04:51 +0900

    axios 테스트.

---
+++ src/AxiosTest.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class AxiosTest extends React.Component {
+  // getData = () => {
+  //   axios
+  //     .get('')
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  render() {
+    return (
+      <div>리액트로 서버와 통신하기</div>
+      // <div>
+      //   <button type="button" onClick={this.getData}></button>
+      // </div>
+    );
+  }
+}
+
+export default AxiosTest;
