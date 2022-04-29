930d15c478d511657a8cc4d0935fb8ac4ad7295e
lecture-bisper <bisper@outlook.kr>
2022-04-29 14:49:21 +0900

    글 쓰기 화면 구현.

--- src/routes/Write.js
+++ src/routes/Write.js
@@ -4,7 +4,65 @@ class Write extends React.Component {
   render() {
     return (
       <div>
-        <h1>쓰기 화면</h1>
+        <header class="container">
+          <div class="p-5 mb-4 bg-light rounded-3 text-center">
+            <div class="container-fluid py-5">
+              <h1 class="display-5 fw-bold">React와 SpringBoot를 활용한 게시판</h1>
+            </div>
+          </div>
+        </header>
+        <main class="container">
+          <section>
+            <article>
+              <div class="row">
+                <div class="col-sm-6 mx-auto">
+                  <div class="my-3">
+                    <label for="title">제목 : </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="title"
+                      name="title"
+                      placeholder="제목을 입력해주세요"
+                    ></input>
+                  </div>
+                  <div class="my-3">
+                    <label for="user-id">ID : </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="user-id"
+                      name="user-id"
+                      placeholder="사용자의 id를 입력하세요"
+                    ></input>
+                  </div>
+                  <div class="my-3">
+                    <label for="content">글 내용 : </label>
+                    <textarea
+                      rows="10"
+                      class="form-control"
+                      id="content"
+                      name="content"
+                      placeholder="간단한 내용을 입력하세요"
+                    ></textarea>
+                  </div>
+                  <div class="my-3 d-flex justify-content-between">
+                    <div>
+                      <button class="btn btn-secondary">목록</button>
+                    </div>
+                    <div>
+                      <button class="btn btn-primary mx-3">등록</button>
+                      <button class="btn btn-warning">취소</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </section>
+        </main>
+        <footer class="container-fluid mt-5 py-5 border-top">
+          <p class="lead text-mute text-center">made by bitc</p>
+        </footer>
       </div>
     );
   }
