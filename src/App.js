a6575167f24647e8de6d76edb2a162cbf41e495a
lecture-bisper <bisper@outlook.kr>
2022-04-29 14:07:03 +0900

    axios 테스트.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 // import logo from './logo.svg';
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+import Write from './routes/Write';
+import Navigation from './components/Navigation';
+
 import './App.css';
-import AxiosTest from './AxiosTest';
+
+// import AxiosTest from './AxiosTest';
 
 function App() {
   return (
-    <div className="App">
-      <AxiosTest />
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/detail" component={Detail} />
+      <Route path="/write" component={Write} />
+    </HashRouter>
+    // <div className="App">
+    //   <AxiosTest />
+    // </div>
   );
 }
 
